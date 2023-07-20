a4679df0c2286c2827dc865ddee9903ad88f624b
Benjamin Webster <ben@Benjamins-MacBook-Pro.local>
2023-07-20 11:24:35 -0400

    updated entry point in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    version="0.1.3",
+    version="0.1.4",
     name="astrotitles",
     packages=find_packages(),
     license="AGPL-3.0",
@@ -19,7 +19,7 @@ setup(
     long_description=long_description,
     long_description_content_type="markdown",
     entry_points={
-        'console_scripts': ['astrotitles=astrotitles.cli:main'],
+        'console_scripts': ['astrotitles=astrotitles.cli:cli'],
     },
     include_package_data=True,
 )
